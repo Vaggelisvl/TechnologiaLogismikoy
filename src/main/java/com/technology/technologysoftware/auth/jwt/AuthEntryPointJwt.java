f37aa36973e461a4cc0b4144c458b3773c9a9d10
e.vrailas <e.vrailas@pmmretail.com>
2023-07-05 15:03:46 +0300

    Optimize imports Code clean-up Remove unnecessary comments

--- src/main/java/com/technology/technologysoftware/auth/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/technology/technologysoftware/auth/jwt/AuthEntryPointJwt.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -37,7 +36,6 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), body);
 
-//    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
 }
 
